c483a861780a7fcd1e1e93067cdae369fd1ee78c
mprahl <matt_prahl@live.com>
2017-05-11 18:02:35 -0400

    Fix filtering on pdc.apps.componentbranch

--- pdc/apps/componentbranch/filters.py
+++ pdc/apps/componentbranch/filters.py
@@ -13,16 +13,15 @@ from pdc.apps.componentbranch.models import (
 
 class ComponentBranchFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(name='name', lookup_type='iexact')
-    global_component__name = django_filters.CharFilter(
-        name='global_component', lookup_type='iexact')
-    type__name = django_filters.CharFilter(name='type', lookup_type='iexact')
+    global_component = django_filters.CharFilter(
+        name='global_component__name', lookup_type='iexact')
+    type = django_filters.CharFilter(
+        name='type__name', lookup_type='iexact')
     active = CaseInsensitiveBooleanFilter()
     critical_path = CaseInsensitiveBooleanFilter()
 
     class Meta:
         model = ComponentBranch
-        # Specifies the exact lookups to allow
-        fields = ('name', 'global_component', 'type')
 
 
 class SLAFilter(django_filters.FilterSet):
@@ -39,11 +38,11 @@ class SLAToComponentBranchFilter(django_filters.FilterSet):
     global_component = django_filters.CharFilter(name='branch__global_component__name', lookup_type='iexact')
     branch_type = django_filters.CharFilter(name='branch__type__name', lookup_type='iexact')
     branch_active = CaseInsensitiveBooleanFilter(name='branch__active', lookup_type='iexact')
+    branch_critical_path = CaseInsensitiveBooleanFilter(name='branch__critical_path', lookup_type='iexact')
     eol_after = django_filters.DateFilter(name="eol", lookup_type='gte')
     eol_before = django_filters.DateFilter(name="eol", lookup_type='lte')
 
     class Meta:
         model = SLAToComponentBranch
         # Specifies the exact lookups to allow
-        fields = ('sla', 'branch', 'global_component', 'branch_type', 'eol',
-                  'eol_after', 'eol_before')
+        fields = ('eol',)
